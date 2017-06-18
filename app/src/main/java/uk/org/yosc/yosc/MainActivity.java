fb1ed6050b3cfbf008235abe2147765ca20f37df
Brian <brianpickles@gmail.com>
2017-06-18 20:33:53 +0100

    Improved main screen layout, improved nav drawer colours and added all this seasons sailing events

--- app/src/main/java/uk/org/yosc/yosc/MainActivity.java
+++ app/src/main/java/uk/org/yosc/yosc/MainActivity.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 
 import android.support.design.widget.NavigationView;
 
@@ -17,23 +15,24 @@ import android.support.v4.widget.DrawerLayout;
 //import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
+        android.support.v7.app.ActionBar ab = getSupportActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+/*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,12 +41,13 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
-
+*/
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this, drawer,  R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -55,6 +55,7 @@ public class MainActivity extends AppCompatActivity
 
 
     }
+
 
     @Override
     public void onBackPressed() {
@@ -82,6 +83,10 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
--- app/src/main/java/uk/org/yosc/yosc/SailingEventsActivity.java
+++ app/src/main/java/uk/org/yosc/yosc/SailingEventsActivity.java
@@ -50,10 +50,31 @@ public class SailingEventsActivity extends AppCompatActivity {
     private void prepareEvents() {
 
 
-        Event a = new Event("Mon 31 June 2017", "Newtown Creek Rally");
+        Event a = new Event("10-11 June 2017", "Newtown Beech Barbecue");
         eventList.add(a);
 
-        a = new Event("Sat/Sun 15/16 July 2017", "Chichester Rally");
+        a = new Event("24 June to 8 July 2017", "Summer Cruise to Brittany");
+        eventList.add(a);
+
+        a = new Event("22-24 July 2017", "Wight Trophy - Round The Island sailing time trial in association with other local yacht clubs with dinner at the Royal Solent Yacht Club, Yarmouth");
+        eventList.add(a);
+
+        a = new Event("5-6 August 2017", "Lymington Rally");
+        eventList.add(a);
+
+        a = new Event("19 to 28 August 2017", "Autumn Cruise westwards to Weymouth");
+        eventList.add(a);
+
+        a = new Event("9-10 September 2017", "Littlehampton Rally");
+        eventList.add(a);
+
+        a = new Event("13-24 September 2017", "Beaulieu River Rally to Ginns Farm pontoon");
+        eventList.add(a);
+
+        a = new Event("7-8 October 2017", "Chichester Rally");
+        eventList.add(a);
+
+        a = new Event("21-22 October 2017", "Laying Up Supper");
         eventList.add(a);
 
         mAdapter.notifyDataSetChanged();
